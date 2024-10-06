e2cbdbedfdfc1dc981cb578f4ee549343856b54a
MikeD76 <madais@csuchico.edu>
2024-10-06 16:54:09 -0700

    Finished main file to return JSON object that meets expectations

--- liapp/liapp.js
+++ liapp/liapp.js
@@ -1,11 +1,39 @@
+// Import the express module. Minimal and flexible Node.js web app framework.
+// require() loads express library for use in application
 const express = require('express')
+
+// Express function is a top-level function exported by the express module
+// app represents the web application. Provides methods for defining routes
+// and handling requests
 const app = express()
+
+
+// Sets the port number to 3000. Server will listen here for incoming requests
 const port = 3000
 
+
+// Defines route handler for HTTP GET requests made to root URL '/'
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+  // Get current timestamp. Date().getTime returns current timestamp in milliseconds since
+  // January 1, 1970 (Unix epoch - starting point for measuring time in Unix systems).
+  // Gives time Dynamically
+  const timestamp = new Date().getTime();
+
+  // Message object created inside route handler.
+  const message =
+  {
+    "message": "My name is Michael Davis",
+    "timestamp": timestamp
+  }
+
+  // sends message object as a response to the client. Sends object in JSON format by default
+  res.send(message)
 })
 
+// listen for incoming requests on port (3000).
 app.listen(port, () => {
+  // message below is shown on console to show that server has started successfully
+  // and is ready to handle requests
   console.log(`Example app listening on port ${port}`)
 })
