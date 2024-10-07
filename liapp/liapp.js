f800a2697c879ad096bb5851567639765a58658c
MikeD76 <madais@csuchico.edu>
2024-10-06 22:46:54 -0700

    used stringify function on message to try and minify the object

--- liapp/liapp.js
+++ liapp/liapp.js
@@ -14,18 +14,20 @@ const port = 80
 
 // Defines route handler for HTTP GET requests made to root URL '/'
 app.get('/', (req, res) => {
-
+  
   // Get current timestamp. Date().getTime returns current timestamp in milliseconds since
   // January 1, 1970 (Unix epoch - starting point for measuring time in Unix systems).
   // Gives time Dynamically, creates new timestamp everytime window is refreshed
   const timestamp = new Date().getTime();
-
+  
   // Message object created inside route handler.
   const message =
   {
     "message": "My name is Michael Davis",
     "timestamp": timestamp
   }
+
+  const mini = JSON.stringify(message)
 
   // sends message object as a response to the client. Sends object in JSON format by default
   res.send(message)
