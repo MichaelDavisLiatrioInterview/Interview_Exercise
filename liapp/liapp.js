99575af915e24c1b9e4ba0dc19e2b311b035c97c
MikeD76 <madais@csuchico.edu>
2024-10-23 15:10:15 -0700

    test commit

--- liapp/liapp.js
+++ liapp/liapp.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
   {
     "message": "My name is Michael Davis",
     "timestamp": timestamp,
-    "Thank you": "for watching!"
+    "I am on": "a Google Slide!"
   }
 
   const mini = JSON.stringify(message)
