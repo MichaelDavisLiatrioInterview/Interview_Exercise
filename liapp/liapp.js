0f292600a2baa687117cef0616d6eca7df10034b
MikeD76 <madais@csuchico.edu>
2024-10-24 12:24:35 -0700

    practice demo 10876

--- liapp/liapp.js
+++ liapp/liapp.js
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
   {
     "message": "My name is Michael Davis",
     "timestamp": timestamp,
-    "I am on": "whoooooo!"
+    "I am on": "here we go!"
   }
 
   const mini = JSON.stringify(message)
